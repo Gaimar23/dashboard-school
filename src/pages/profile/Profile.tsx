8a08f614505f5779d8de45fbbd2fc827511a60e4
Gaimar23 <highdevelopment01@gmail.com>
2025-04-27 02:44:04 +0200

    Commit tenth App update

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -2,6 +2,55 @@ import Menu from "../../components/menu/Menu";
 import Navbar from "../../components/navbar/Navbar";
 import "./Profile.scss";
 import photo from "../../assets/images/john.jpg";
+import Select from "react-select";
+import { FaLocationDot } from "react-icons/fa6";
+import { ImCalendar } from "react-icons/im";
+
+const options = [
+  {
+    value: "chocolate",
+    label: "Chocolate",
+  },
+  {
+    value: "strawberry",
+    label: "Straw",
+  },
+  {
+    value: "vanilla",
+    label: "Vanilla",
+  },
+  {
+    value: "economie",
+    label: "Economie",
+  },
+  {
+    value: "droit",
+    label: "Droit des affaires",
+  },
+  {
+    value: "comptabilité",
+    label: "Comptabilité financière",
+  },
+  {
+    value: "math_applique",
+    label: "Mathématique appliqué",
+  },
+  {
+    value: "entrepreneuriat",
+    label: "Entrepreneuriat",
+  },
+];
+
+const genre = [
+  {
+    value: "man",
+    label: "Homme",
+  },
+  {
+    value: "woman",
+    label: "Femme",
+  },
+];
 
 const Profile = () => {
   return (
@@ -17,50 +66,131 @@ const Profile = () => {
               </div>
 
               <div className="info">
-                <div className="row flex">
-                  <div className="sub-row">
-                    <label htmlFor="">N°ID</label>
+                <div
+                  style={{
+                    backgroundColor: "#F7F8FA",
+                    padding: "16px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                    gap: "6px",
+                    borderRadius: "6px",
+                  }}
+                >
+                  <div className="row flex">
+                    <div className="sub-row">
+                      <label htmlFor="">N°ID</label>
+                      <input type="text" />
+                    </div>
+                    <div className="sub-row">
+                      <label htmlFor="">Nom</label>
+                      <input type="text" />
+                    </div>
+                  </div>
+                  <div className="row flex">
+                    <div className="sub-row">
+                      <label htmlFor="">Email</label>
+                      <input type="text" />
+                    </div>
+                    <div className="sub-row">
+                      <label htmlFor="">Mot de passe</label>
+                      <input type="text" />
+                    </div>
+                  </div>
+                </div>
+
+                <div
+                  style={{
+                    backgroundColor: "#F7F8FA",
+                    padding: "16px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                    gap: "6px",
+                    borderRadius: "6px",
+                    marginTop: "20px",
+                  }}
+                >
+                  <div className="row">
+                    <label htmlFor="">Adresse</label>
                     <input type="text" />
                   </div>
-                  <div className="sub-row">
-                    <label htmlFor="">Nom</label>
-                    <input type="text" />
+                  <div className="row flex">
+                    <div className="sub-row">
+                      <label htmlFor="">Phone</label>
+                      <input type="text" />
+                    </div>
+                    <div className="sub-row">
+                      <label htmlFor="">Genre</label>
+                      <input type="text" />
+                    </div>
+                  </div>
+                  <div className="row">
+                    <label htmlFor="">Classes</label>
+                    <Select
+                      options={options}
+                      isMulti
+                      styles={{
+                        menuList: () => ({
+                          maxHeight: "150px",
+                          overflowY: "scroll",
+                          scrollBehavior: "smooth",
+                          padding: "3px",
+                        }),
+                      }}
+                    />
+                  </div>
+                  <div className="row">
+                    <label htmlFor="">Sujets</label>
+                    <Select
+                      options={options}
+                      isMulti
+                      styles={{
+                        menuList: () => ({
+                          maxHeight: "80px",
+                          overflowY: "scroll",
+                          scrollBehavior: "smooth",
+                          padding: "3px",
+                        }),
+                      }}
+                    />
                   </div>
                 </div>
-                <div className="row">
-                  <label htmlFor="">Email</label>
-                  <input type="email" />
-                </div>
-                <div className="row">
-                  <label htmlFor="">Mot de passe</label>
-                  <input type="password" />
-                </div>
-                <div className="row">
-                  <label htmlFor="">Adresse</label>
-                  <input type="text" />
-                </div>
-                <div className="row flex">
-                  <div className="sub-row">
-                    <label htmlFor="">Phone</label>
-                    <input type="text" />
-                  </div>
-                  <div className="sub-row">
-                    <label htmlFor="">Genre</label>
-                    <input type="text" />
-                  </div>
-                </div>
-                <div className="row">
-                  <label htmlFor="">Classes</label>
-                  <input type="text" />
-                </div>
-                <div className="row">
-                  <label htmlFor="">Sujets</label>
-                  <input type="text" />
-                </div>
+              </div>
+
+              <div className="button-container">
+                <button type="submit">Enregistrer</button>
               </div>
             </form>
           </div>
-          <div className="right"></div>
+          <div className="right">
+            <div className="card">
+              <div className="icons">
+                <ImCalendar className="icon" />
+                <FaLocationDot className="icon" />
+              </div>
+              <h1>102</h1>
+              <span>Parents</span>
+            </div>
+            <div className="card">
+              <div className="icons">
+                <ImCalendar className="icon" />
+                <FaLocationDot className="icon" />
+              </div>
+              <h1>102</h1>
+              <span>Teachers</span>
+            </div>
+            <div className="card">
+              <div className="icons">
+                <ImCalendar className="icon" />
+                <FaLocationDot className="icon" />
+              </div>
+              <h1>102</h1>
+              <span>Students</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -1,13 +1,181 @@
 import Menu from "../../components/menu/Menu";
-import Navbar from "../../components/navbar/Navbar";
 import "./Settings.scss";
+import Select from "react-select";
+import Logo from "../../assets/images/john.jpg";
 
 const Settings = () => {
+  const genre = [
+    {
+      value: 2020,
+      label: "2020",
+    },
+    {
+      value: 2021,
+      label: "2021",
+    },
+    {
+      value: 2022,
+      label: "2022",
+    },
+    {
+      value: 2023,
+      label: "2023",
+    },
+    {
+      value: 2024,
+      label: "2024",
+    },
+    {
+      value: 2025,
+      label: "2025",
+    },
+  ];
+
   return (
     <div className="settings-container">
       <Menu />
       <div className="right">
-        <Navbar />
+        <div className="settings">
+          <div className="title-container">
+            <span className="title">Paramètres</span>
+          </div>
+
+          <div className="general">
+            <div className="top">
+              <span>01</span>
+              <h1>Général</h1>
+            </div>
+            <div className="bottom">
+              <div className="column">
+                <h4>Language</h4>
+                <div className="language-container">
+                  <div className="row">
+                    <input type="checkbox" name="" />
+                    <span>French</span>
+                  </div>
+                  <div className="row">
+                    <input type="checkbox" name="" />
+                    <span>English</span>
+                  </div>
+                </div>
+              </div>
+              <div className="column">
+                <h4>Academic Year</h4>
+                <div className="year-container">
+                  <div className="row">
+                    <span>Start</span>
+                    <Select
+                      options={genre}
+                      styles={{
+                        menuList: () => ({
+                          maxHeight: "150px",
+                          overflowY: "scroll",
+                          scrollBehavior: "smooth",
+                          padding: "3px",
+                        }),
+                      }}
+                    />
+                  </div>
+                  <div className="row">
+                    <span>End</span>
+                    <Select
+                      options={genre}
+                      styles={{
+                        menuList: () => ({
+                          maxHeight: "150px",
+                          overflowY: "scroll",
+                          scrollBehavior: "smooth",
+                          padding: "3px",
+                        }),
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="school-info">
+            <div className="top">
+              <span>02</span>
+              <h1>School Info</h1>
+            </div>
+            <div className="bottom">
+              <div className="row first">
+                <div className="image-container">
+                  <img src={Logo} alt="" />
+                </div>
+                <div className="inputs-container">
+                  <input type="text" placeholder="School name" />
+                  <input type="email" name="" placeholder="Email" />
+                </div>
+              </div>
+
+              <div className="row second">
+                <input type="text" placeholder="Téléphone" />
+                <input type="text" name="" placeholder="Adresse" />
+              </div>
+              <div className="row third">
+                <input type="text" placeholder="Principal" />
+                <input type="text" name="" placeholder="Devise" />
+              </div>
+            </div>
+          </div>
+          <div className="users-roles">
+            <div className="top">
+              <span>03</span>
+              <h1>Users & roles</h1>
+            </div>
+            <div className="bottom">
+              <div className="column">
+                <h4>Roles</h4>
+                <div className="roles-container">
+                  <div className="first">
+                    <div className="row">
+                      <input type="checkbox" name="" />
+                      <span>Admin</span>
+                    </div>
+                    <div className="row">
+                      <input type="checkbox" name="" />
+                      <span>Teacher</span>
+                    </div>
+                  </div>
+                  <div className="second">
+                    <div className="row">
+                      <input type="checkbox" name="" />
+                      <span>Student</span>
+                    </div>
+                    <div className="row">
+                      <input type="checkbox" name="" />
+                      <span>Parent</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="column">
+                <h4>Invitations</h4>
+                <div className="invitations-container">
+                  <input type="text" placeholder="@ adresse email" />
+                  <button>Envoyer</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="notifications">
+            <div className="top">
+              <span>04</span>
+              <h1>Notifications</h1>
+            </div>
+            <div className="bottom">
+              <div className="column">
+                <h4>Notification</h4>
+                <div className="row">
+                  <input type="checkbox" name="" />
+                  <span>Email/Sms</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
